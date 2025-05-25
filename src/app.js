95109ff2a2691892f1338e769f5c4ea7bda49b50
Sofia <sofia.arone@sou.inteli.edu.br>
2025-05-25 14:14:17 -0300

    fix: arruma porta 3000 e página html

--- src/app.js
+++ src/app.js
@@ -3,16 +3,25 @@ const path = require('path');
 
 const app = express();
 
+// Configura o EJS como view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views')); // Define o caminho para as views
+
 // Log para todas as requisições
 app.use((req, res, next) => {
     console.log(`${req.method} ${req.url}`);
     next();
 });
 
-// Rota mais simples para teste
+// Rota principal usando EJS
 app.get('/', (req, res) => {
     console.log('Rota principal acessada');
-    res.send('<h1>Olá! O servidor está funcionando!</h1>');
+    res.render('index'); // Renderiza views/index.ejs
+});
+
+// Rota extra usando home.ejs
+app.get('/home', (req, res) => {
+    res.render('home');
 });
 
 const PORT = 3000;
